69d2979f674720a6d2dfe93a2c9ab85be3e54c07
Ben Ryan <50218096+benryan03@users.noreply.github.com>
2020-09-09 23:10:10 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using HtmlAgilityPack;
+using System;
+
+namespace MLB_Trade_Alerts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MLB TRADE ALERTS");
+            Console.WriteLine(DateTime.Now);
+
+            //Console.WriteLine("Enter team: ");
+            //string Team = Console.ReadLine();
+            //Console.WriteLine(Team);
+
+            var url = "https://www.mlb.com/redsox/roster/transactions/2020/09/";
+            var web = new HtmlWeb();
+            var doc = web.Load(url);
+            var nodes = doc.DocumentNode.SelectNodes("//table[@class='roster__table']/tbody/tr/td");
+
+            foreach (var x in nodes)
+            {
+                Console.WriteLine(x.InnerText);
+            }
+        }
+    }
+}
