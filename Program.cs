630a29922d5845fea78cead948dfc40ebfde4f38
Ben Ryan <benryan03@gmail.com>
2020-09-10 21:55:46 -0400

    progress

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using HtmlAgilityPack;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MLB_Trade_Alerts
 {
@@ -8,7 +10,9 @@ namespace MLB_Trade_Alerts
         static void Main(string[] args)
         {
             Console.WriteLine("MLB TRADE ALERTS");
-            Console.WriteLine(DateTime.Now);
+
+            string currentDate = DateTime.Now.ToString("MM/dd/yy"); ;
+            Console.WriteLine(currentDate + "\n");
 
             //Console.WriteLine("Enter team: ");
             //string Team = Console.ReadLine();
@@ -19,9 +23,32 @@ namespace MLB_Trade_Alerts
             var doc = web.Load(url);
             var nodes = doc.DocumentNode.SelectNodes("//table[@class='roster__table']/tbody/tr/td");
 
+            List<string> datesList = new List<string>();
+            List<string> tradesList = new List<string>();
+
+            int iteration = 0;
             foreach (var x in nodes)
             {
-                Console.WriteLine(x.InnerText);
+                iteration++;
+                if (iteration % 2 != 0) // Odd
+                {
+                    datesList.Add(x.InnerText);
+                }
+                else // Even
+                {
+                    tradesList.Add(x.InnerText);
+                }    
+            }
+
+            int tradeQuantity = tradesList.Count;
+
+            for (int x = 0; x < tradeQuantity; x++)
+            {
+                if (datesList[x] == currentDate)
+                {
+                    Console.WriteLine(datesList[x]);
+                    Console.WriteLine(tradesList[x]);
+                }
             }
         }
     }
